4388894c22996f4fd5f6c77d074dad89c2248cb7
Hiten Patel <Hiten@hitens-macbook-pro.local>
2019-03-06 11:43:51 -0500

    Initial Commit

---
+++ FifaWorldCup/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FifaWorldCup
+//
+//  Created by Hiten Patel on 2019-03-06.
+//  Copyright © 2019 MAD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
